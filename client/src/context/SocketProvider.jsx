452ac514f2bb46b19d6fbd13ed33e8af6f48e6ad
Aryan Verma <68707819+AR13202@users.noreply.github.com>
2024-10-26 12:13:45 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) =>{
-    const socket = useMemo(()=>io("localhost:8000"),[]);
+    const socket = useMemo(()=>io("https://temp-video-call.onrender.com"),[]);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
